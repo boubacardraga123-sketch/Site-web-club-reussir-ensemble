b0e02b8c190b1a71b1cee26faba78ec6cd1d94df
boubacardraga123-sketch <boubacardraga123@gmail.com>
2026-08-09 09:46:20 +0000

    Add service worker for asset caching
    Implement service worker for caching core assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'cre-site-v1';
+const CORE_ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Réseau d'abord (données Firestore toujours à jour), repli sur le cache hors-ligne
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
